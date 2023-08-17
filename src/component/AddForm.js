c96802d452bb8c92415995e76b69e127c3a41d92
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-08-17 07:30:33 -0400

    add

--- src/component/AddForm.js
+++ src/component/AddForm.js
@@ -1,6 +1,8 @@
 import { background } from '@chakra-ui/react';
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
+import './Addform.css';
+
 
 const AddForm = ({keyAddTask}) => {
     //this use state to hold input value
@@ -10,26 +12,17 @@ const AddForm = ({keyAddTask}) => {
       keyAddTask(inputValue)
       
     }
-    const styles = {
-    
-     button: {
-        padding: "10px",
-        color: "red",
-        margin:"auto",
-        display:"block",
-        backgroundColor: "pink"
-      },
-    };
+   
   return (
     // button send all data that user typing to usestatein App.js
     <div className='container d-flex flex-column'>
-        <h2>Track Expense</h2>
-<div className='mt-3 mx-5'>
+        <h2 className='text-center'>Expense Tracking</h2>
+<div className='mt-3 mx-5 targetInput'>
     {/* below onchange do change value and install inside value */}
-<input type="text" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} />
+<input type="text" onChange={(e)=> setInputValue(e.target.value)} value={inputValue}  />
 </div>
-<div className='ml-5'>
-    <Button onClick={handleAddTask} className='targetButton'  style={styles.button}>Add Items</Button>
+<div className='ml-5 targetButton'>
+    <Button onClick={handleAddTask}>Add Items</Button>
 </div>
 
     </div>
